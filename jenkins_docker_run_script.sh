ae9bcce78f57f7ae871aa687742f6ba24689f3d3
Julien Nyambal <julienn@hyperiondev.com>
2024-06-17 20:29:09 +0200

    adds dockerfile agent

--- jenkins_docker_run_script.sh
+++ jenkins_docker_run_script.sh
@@ -1,8 +1,3 @@
-# simple_jenkins
-
-Run 
-
-```bash
 docker run `
   --name jenkins-blueocean `
   --restart=on-failure --detach `
@@ -14,8 +9,3 @@ docker run `
   --volume jenkins-docker-certs:/certs/client:ro `
   --publish 8080:8080 `
   --publish 50000:50000 custom_jenkins
-```
-
-```bash
-docker run -d --restart=always -p 127.0.0.1:2376:2375 --network jenkins --volume /var/run/docker.sock:/var/run/docker.sock alpine/socat tcp-listen:2375,fork,reuseaddr unix-connect:/var/run/docker.sock
-```
